1d69717116da44717c9a5106750d2562c5a9e405
fujitawataru <fuji3301@icloud.com>
2022-02-27 06:10:31 +0000

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!,except: [:about]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  
+
+
   def after_sign_up_path_for(resource)
-     user_path(@user.id)
+     user_path(current_user)
   end
-  
+
   def after_sign_in_path_for(resource)
-     user_path(@user.id)
+     user_path(current_user)
   end
 
   protected
